819b63bc7b6656067862860f07cade815264b784
siqueirajoaovitor <github.joao.vitor.siqueira@gmail.com>
2026-07-17 11:09:54 -0300

    adiciona exercício de maior numero

---
+++ maior-numero.py
@@ -0,0 +1,8 @@
+numero1 = int(input("digite o primeiro numero: "))
+
+numero2 = int(input("digite o segundo numero: "))
+
+if numero1 > numero2:
+    print("o primeiro numero é maior que o segundo")
+else:
+    print("o segundo numero é mair que o primeiro ")
