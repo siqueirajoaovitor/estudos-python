57861c362b0d1daceabfeb34cab68e38a445fb41
siqueirajoaovitor <github.joao.vitor.siqueira@gmail.com>
2026-07-17 11:12:18 -0300

    adiciona exercício de par ou ímpar

---
+++ par-ou-impar.py
@@ -0,0 +1,8 @@
+print("Olá, seja bem vindo ao nosso par ou impar")
+
+numero = int(input("digite o numero que vcê quer saber se é par ou impar: "))
+
+if numero % 2 == 0:
+    print("o numero é par")
+else:
+    print("o numero é impar")
